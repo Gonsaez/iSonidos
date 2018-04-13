5e82c51c596060e1eb58dd554956e757a95d2a16
xp <gon98saez@gmail.com>
2018-04-13 18:14:35 +0200

    Coge todos los botones con sus respectivos sonidos. En un futuro cambiare el soundpool por el mediaplayer para que me coja todos los tamaños de mp3.

--- app/src/main/java/com/example/xp/isonidos/MainActivity.java
+++ app/src/main/java/com/example/xp/isonidos/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.xp.isonidos;
 
+import android.graphics.Color;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.media.SoundPool;
@@ -7,17 +8,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+
+import java.lang.reflect.Field;
 
 public class MainActivity extends AppCompatActivity {
     SoundPool sonidos;
     int sonidon1;
-    int[] su = new int[57];
-    int sonidon2;
-    int sonidon3;
-    int sonidon4;
-    int sonidon5;
-    int sonidon6;
-    int sonidon7;
+    Field[] nombreC = R.raw.class.getFields();
+    int[] su = new int[nombreC.length];
+    int idC;
 
 
     @Override
@@ -25,30 +25,49 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         sonidos = new SoundPool(4, AudioManager.STREAM_MUSIC, 0);
-        sonidon1 = sonidos.load(this, R.raw.inflar, 1);
-        sonidon2 = sonidos.load(this, R.raw.memociono, 1);
-        sonidon3 = sonidos.load(this, R.raw.oh_ma_gawd, 1);
-        sonidon4 = sonidos.load(this, R.raw.bitesdedust, 1);
-        sonidon5 = sonidos.load(this, R.raw.pedrerol_ya_esta_bien, 1);
-        sonidon6 = sonidos.load(this, R.raw.notte, 1);
-        sonidon7 = sonidos.load(this, R.raw.al_carrer, 1);
-    }
 
-    public void sonido(View vista){
-        Button boton = (Button) findViewById(vista.getId());
-        int numeroSonido = Integer.valueOf(boton.getTag().toString());
-        switch (numeroSonido){
-            case 0: sonidos.play(sonidon1,1,1,1,0,1f);  break;
-            case 1: sonidos.play(sonidon2,1,1,1,0,1f); break;
-            case 2: sonidos.play(sonidon3,1,1,1,0,1f); break;
-            case 3: sonidos.play(sonidon4,1,1,1,0,1f); break;
-            case 4: sonidos.play(sonidon5,1,1,1,0,1f); break;
-            case 5: sonidos.play(sonidon6,1,1,1,0,1f); break;
-            case 6: MediaPlayer mp = MediaPlayer.create(this, R.raw.al_carrer ); mp.start();break;
-            default: sonidos.play(sonidon1,1,1,1,0,1f);  break;
+
+        //Selecciono el linerlayout
+        LinearLayout principal = (LinearLayout) findViewById(R.id.botones);
+        for(int i = 0; i < nombreC.length; i++){
+            //creamos un botón por código y lo añadimos a la pantalla principal
+            Button b = new Button(this);
+            b.setText(nombreC[i].getName());
+            b.setTextColor(Color.WHITE);
+            b.setBackgroundColor(Color.BLUE);
+            b.setAllCaps(false);//todas las letras del botón en minúscula
+            b.setTag(i+1);
+            b.setId(i);
+            b.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    sonido(v);
+                }
+            });
+
+            //añadimos el botón al layout
+            principal.addView(b);
         }
 
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        for (int i = 0; i < su.length; i++) {
+            try {
+                idC = nombreC[i].getInt(nombreC[i]);
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+            su[i] = sonidos.load(this, idC , 1);
+        }
+    }
+
+    public void sonido(View vista) {
+        Button boton = (Button) findViewById(vista.getId());
+        int numeroSonido = Integer.valueOf(boton.getTag().toString()) - 1;
+        sonidos.play(su[numeroSonido], 1, 1, 1, 0, 1);
     }
 
 
